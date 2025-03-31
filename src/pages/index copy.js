ee283c9f6493c218318bea9e3a38837c7c1eedc9
soichiro-matz <matz.fukuoka@gmail.com>
2025-03-31 15:54:36 +0900

    エラー対応

--- src/pages/index copy.js
+++ src/pages/index copy.js
@@ -1,47 +1,7 @@
 export default function Home() {
   return (
     <ul class="nav">
-      <li class="parent">
-        <a href="/">
-          <p class="title_en">Home</p>
-          <p class="title">ホーム</p>
-        </a>
-        <div class="child">
-          <ul>
-            <li>
-              <a href="#">first</a>
-            </li>
-            <li>
-              <a href="#">second</a>
-            </li>
-            <li>
-              <a href="#">third</a>
-            </li>
-          </ul>
-        </div>
-      </li>
-      <li class="parent">
-        <a href="about">
-          <p class="title_en">About</p>
-          <p class="title">企業情報</p>
-        </a>
-        <div class="child">
-          <ul>
-            <li>
-              <a href="#">vvm</a>
-            </li>
-            <li>
-              <a href="#">outline</a>
-            </li>
-          </ul>
-        </div>
-      </li>
-      <li class="parent">
-        <a href="service">
-          <p class="title_en">Service</p>
-          <p class="title">サービス</p>
-        </a>
-      </li>
+      <li class="parent"></li>
     </ul>
   );
 }
