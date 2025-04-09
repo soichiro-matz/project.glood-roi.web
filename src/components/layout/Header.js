49cf2d3ad9a76caba8627eb8900b25a266aee5ae
soichiro-matz <matz.fukuoka@gmail.com>
2025-04-09 13:47:10 +0900

    haader レイアウト調整

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -278,133 +278,130 @@ export default function Header() {
   };
 
   return (
-    <header className="fixed left-0 top-0 z-[100] w-full bg-white">
-      <div className="p-header mx-auto flex items-center justify-between">
-        <h1 className="z-[100] text-xl font-bold">
-          <Link href="/">
-            <Image
-              src="/assets/img/common/logo.png"
-              alt="株式会社Glood roi"
-              height={113}
-              width={554}
-              className="h-fluid-[24,28,365,767] w-fluid-[117,136,365,767] md:h-fluid-[28,32,768,1024] md:w-fluid-[136,161,768,1024]"
-            />
-          </Link>
-        </h1>
-
-        <nav
-          id="main-navigation"
-          className={`nav-menus js-nav-menus z-30 overflow-y-scroll pt-fluid-[64,72,350,1024] lg:h-full lg:overflow-y-visible lg:pt-0 ${isOpen ? "open" : ""}`}
-          role="navigation"
-          aria-label="メインナビゲーション"
-        >
-          <ul className="flex h-fit w-full flex-col space-y-6 rounded-2xl bg-white px-7 py-10 text-center lg:h-full lg:flex-row lg:gap-x-8 lg:space-y-0 lg:p-0">
-            {Object.entries(navLinks).map(([key, link]) => {
-              const pcHiddenMenus = ["home"]; //PC版で非表示（複数指定可）
-              const isPcHidden = pcHiddenMenus.includes(key);
-              const hasChildren = link.children && link.children.length > 0;
-              const isActive = activeMenus.includes(key);
-              const isHiddenMenus = ["privacyPolicy"]; //ヘッダーメニューで非表示（複数指定可）
-              const isHidden = isHiddenMenus.includes(key);
-              const isContact = key === "contact";
-              const isFocused = focusedMenu === key && isLg;
-
-              return isHidden ? (
-                ""
-              ) : (
-                <li
-                  key={link.href}
-                  className={`nav-menu js-nav-menu relative flex-col justify-center ${isPcHidden ? "lg:hidden" : ""} ${isContact ? "hidden lg:flex" : "flex"}`}
-                  tabIndex={isLg && hasChildren ? 0 : -1}
-                  // onMouseEnter={() => handleMouseEnter(key)}
-                  // onMouseLeave={() => handleMouseLeave(key)}
-                  onFocus={() => handleFocus(key)}
-                  onBlur={(e) => handleBlur(e, key)}
-                  data-key={`${key}`}
-
-                  // onClick={() => handleItemClick(link.href)}  // ここでページ遷移を実行
-                >
-                  <div className="flex w-full items-center justify-between">
-                    <Link
-                      href={link.href}
-                      className={`nav-link block items-center text-left lg:flex ${hasChildren ? "has-children" : ""} ${key === "contact" ? "isContact" : ""}`}
-                      onClick={() => setIsOpen(false)}
-                    >
-                      <p
-                        className={`title-en js-title-en u-clip__full font-bold leading-none pb-fluid-[6,6,350,768] text-fluid-[24,26,350,768] lg:hidden`}
+    <header className="fixed left-0 top-0 z-[100] w-full">
+      <div className="l-header__wrapper">
+        <div className="p-header mx-auto flex items-center justify-between">
+          <h1 className="z-[100] text-xl font-bold">
+            <Link href="/">
+              <Image
+                src="/assets/img/common/logo.png"
+                alt="株式会社Glood roi"
+                height={113}
+                width={554}
+                className="h-fluid-[24,28,365,767] w-fluid-[117,136,365,767] md:h-fluid-[28,32,768,1024] md:w-fluid-[136,161,768,1024]"
+              />
+            </Link>
+          </h1>
+          <nav
+            id="main-navigation"
+            className={`nav-menus js-nav-menus z-30 overflow-y-scroll pt-fluid-[64,72,350,1024] lg:h-full lg:overflow-y-visible lg:pt-0 ${isOpen ? "open" : ""}`}
+            role="navigation"
+            aria-label="メインナビゲーション"
+          >
+            <ul className="flex h-fit w-full flex-col space-y-6 rounded-2xl bg-white px-7 py-10 text-center lg:h-full lg:flex-row lg:gap-x-8 lg:space-y-0 lg:bg-transparent lg:p-0">
+              {Object.entries(navLinks).map(([key, link]) => {
+                const pcHiddenMenus = ["home"]; //PC版で非表示（複数指定可）
+                const isPcHidden = pcHiddenMenus.includes(key);
+                const hasChildren = link.children && link.children.length > 0;
+                const isActive = activeMenus.includes(key);
+                const isHiddenMenus = ["privacyPolicy"]; //ヘッダーメニューで非表示（複数指定可）
+                const isHidden = isHiddenMenus.includes(key);
+                const isContact = key === "contact";
+                const isFocused = focusedMenu === key && isLg;
+                return isHidden ? (
+                  ""
+                ) : (
+                  <li
+                    key={link.href}
+                    className={`nav-menu js-nav-menu relative flex-col justify-center ${isPcHidden ? "lg:hidden" : ""} ${isContact ? "hidden lg:flex" : "flex"}`}
+                    tabIndex={isLg && hasChildren ? 0 : -1}
+                    // onMouseEnter={() => handleMouseEnter(key)}
+                    // onMouseLeave={() => handleMouseLeave(key)}
+                    onFocus={() => handleFocus(key)}
+                    onBlur={(e) => handleBlur(e, key)}
+                    data-key={`${key}`}
+                    // onClick={() => handleItemClick(link.href)}  // ここでページ遷移を実行
+                  >
+                    <div className="flex w-full items-center justify-between">
+                      <Link
+                        href={link.href}
+                        className={`nav-link block items-center text-left lg:flex ${hasChildren ? "has-children" : ""} ${key === "contact" ? "isContact" : ""}`}
+                        onClick={() => setIsOpen(false)}
                       >
-                        {link.text_en}
-                      </p>
-                      <p className="title js-title u-clip__full leading-none text-fluid-[10,12,350,768] lg:text-base">
-                        {link.text}
-                      </p>
-                    </Link>
-                    {hasChildren && (
-                      <button
-                        type="button"
-                        onClick={() => toggleAccordion(key)}
-                        className="toggle-button js-toggle-button ml-2 h-[40px] w-[40px] self-start mr-fluid-[-13,-6,350,768] mt-fluid-[-13,-6,350,768] p-fluid-[13,6,350,768] focus:outline-none"
-                        aria-expanded={isActive}
-                        aria-label="Toggle submenu"
-                      >
-                        <span className="toggle-mark relative block h-full w-full" />
-                      </button>
-                    )}
-                  </div>
-
-                  {hasChildren && (
-                    <div
-                      className={`nav-childMenu__wrapper js-childMenu__wrapper ${activeMenus.includes(key) ? "open" : ""}`}
-                      style={{ opacity: isFocused ? 1 : undefined }}
-                      onClick={() => toggleAccordion(key)}
-                    >
-                      <ul className="nav-childMenu flex flex-col gap-y-3">
-                        {link.children.map((child, index) => (
-                          <li key={child.href} tabIndex={-1}>
-                            <Link
-                              href={child.href}
-                              className="nav-childMenu__link block text-left"
-                              onClick={() => setIsOpen(false)}
-                            >
-                              {child.text}
-                            </Link>
-                          </li>
-                        ))}
-                      </ul>
+                        <p
+                          className={`title-en js-title-en u-clip__full font-bold leading-none pb-fluid-[6,6,350,768] text-fluid-[24,26,350,768] lg:hidden`}
+                        >
+                          {link.text_en}
+                        </p>
+                        <p className="title js-title u-clip__full leading-none text-fluid-[10,12,350,768] lg:text-base">
+                          {link.text}
+                        </p>
+                      </Link>
+                      {hasChildren && (
+                        <button
+                          type="button"
+                          onClick={() => toggleAccordion(key)}
+                          className="toggle-button js-toggle-button ml-2 h-[40px] w-[40px] self-start mr-fluid-[-13,-6,350,768] mt-fluid-[-13,-6,350,768] p-fluid-[13,6,350,768] focus:outline-none"
+                          aria-expanded={isActive}
+                          aria-label="Toggle submenu"
+                        >
+                          <span className="toggle-mark relative block h-full w-full" />
+                        </button>
+                      )}
                     </div>
-                  )}
-                </li>
-              );
-            })}
-            <li className="lg:hidden">
-              <div key="contact" className="js-contact-button pt-5 lg:hidden">
-                <Button
-                  tag="a"
-                  linkProps={{ href: "/conact" }}
-                  className="c-button p-button -primary"
-                >
-                  お問い合わせ
-                </Button>
-              </div>
-            </li>
-          </ul>
-        </nav>
-
-        <button
-          className={`hamburger z-40 ${isOpen ? "open" : ""}`}
-          onClick={() => {
-            if (isOpen) {
-              closeDrawerMenu(); // ドロワーメニューを閉じるアニメーション実行
-            } else {
-              setIsOpen(true); // ドロワーメニューを開く
-            }
-          }}
-          aria-expanded={isOpen}
-          aria-label="メニューを開く"
-          aria-controls="main-navigation"
-        >
-          <span className="humbergerButton__line"></span>
-        </button>
+                    {hasChildren && (
+                      <div
+                        className={`nav-childMenu__wrapper js-childMenu__wrapper ${activeMenus.includes(key) ? "open" : ""}`}
+                        style={{ opacity: isFocused ? 1 : undefined }}
+                        onClick={() => toggleAccordion(key)}
+                      >
+                        <ul className="nav-childMenu flex flex-col gap-y-3">
+                          {link.children.map((child, index) => (
+                            <li key={child.href} tabIndex={-1}>
+                              <Link
+                                href={child.href}
+                                className="nav-childMenu__link block text-left"
+                                onClick={() => setIsOpen(false)}
+                              >
+                                {child.text}
+                              </Link>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    )}
+                  </li>
+                );
+              })}
+              <li className="lg:hidden">
+                <div key="contact" className="js-contact-button pt-5 lg:hidden">
+                  <Button
+                    tag="a"
+                    linkProps={{ href: "/conact" }}
+                    className="c-button p-button -primary"
+                  >
+                    お問い合わせ
+                  </Button>
+                </div>
+              </li>
+            </ul>
+          </nav>
+          <button
+            className={`hamburger z-40 ${isOpen ? "open" : ""}`}
+            onClick={() => {
+              if (isOpen) {
+                closeDrawerMenu(); // ドロワーメニューを閉じるアニメーション実行
+              } else {
+                setIsOpen(true); // ドロワーメニューを開く
+              }
+            }}
+            aria-expanded={isOpen}
+            aria-label="メニューを開く"
+            aria-controls="main-navigation"
+          >
+            <span className="humbergerButton__line"></span>
+          </button>
+        </div>
       </div>
     </header>
   );
