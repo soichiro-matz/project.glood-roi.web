dad5fa38a8a024d06593ffd5d02801531be987f6
soichiro-matz <matz.fukuoka@gmail.com>
2025-04-26 09:12:24 +0900

    change midleware

--- middleware.js
+++ middleware.js
@@ -1,22 +1,13 @@
-// middleware.js
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server'
 
 export function middleware(request) {
-  const auth = request.headers.get("authorization");
-  if (auth) {
-    const [user, pass] = Buffer.from(auth.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-    if (user === "admin" && pass === "secret123") return NextResponse.next();
-  }
-  return new NextResponse("Auth Required", {
-    status: 401,
-    headers: {
-      "WWW-Authenticate": 'Basic realm="Secure Area"',
-    },
-  });
-}
+  const hostname = request.headers.get('host') || ''
 
-export const config = {
-  matcher: ["/((?!_next|favicon.ico).*)"],
-};
+  if (hostname.endsWith('.vercel.app')) {
+    const url = request.nextUrl.clone()
+    url.hostname = 'gloodroi.com'
+    return NextResponse.redirect(url)
+  }
+
+  return NextResponse.next()
+}
